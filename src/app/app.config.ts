3fa23d97e973a41b85e66a416ed4e1c4ac5fd1b2
kyupin64 <lewilks.321@gmail.com>
2024-10-25 15:39:34 -0600

    Add firebase for auth and firestore

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,6 +4,19 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
+import { provideFirebaseApp } from '@angular/fire/app'
+import { initializeApp } from 'firebase/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore'
+import { environment } from '../environments/environment';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes), 
+    provideAnimationsAsync(),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
+  ]
 };
