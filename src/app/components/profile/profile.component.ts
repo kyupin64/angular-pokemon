6145b84b9598b5fc324340cb78d0e2f308f68115
kyupin64 <lewilks.321@gmail.com>
2024-11-07 15:04:42 -0700

    Add basic profile page

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MatCardModule } from '@angular/material/card';
+
+import { User } from '../../interfaces/user';
+import { LoginService } from '../../services/login.service';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    MatCardModule
+  ],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent {
+  currentUser: User | null = null;
 
+  constructor(private loginService: LoginService) {}
+
+  ngOnInit() {
+    this.loginService.getCurrentUser().subscribe((currentUser) => {
+      if (currentUser) {
+        this.currentUser = currentUser;
+      }
+    });
+  }
 }
