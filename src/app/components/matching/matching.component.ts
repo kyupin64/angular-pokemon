d2a3bf5a8af14b1fa7cce4a26b63a7fa9b995c2d
kyupin64 <lewilks.321@gmail.com>
2024-11-09 18:47:29 -0700

    Add loading screen and fix initializing cards

--- src/app/components/matching/matching.component.ts
+++ src/app/components/matching/matching.component.ts
@@ -5,7 +5,6 @@ import { FormArray, FormBuilder, FormControl, FormGroup, ReactiveFormsModule } f
 import { MatCardModule } from '@angular/material/card';
 import { CurrentGame } from '../../interfaces/current-game';
 import { GameService } from '../../services/game.service';
-import { CurrentGameCard } from '../../interfaces/current-game-card';
 
 @Component({
   selector: 'app-matching',
@@ -22,9 +21,9 @@ export class MatchingComponent {
   form: FormGroup;
   fb: FormBuilder = new FormBuilder;
   currentGame$: CurrentGame | null = null;
-  cardsArr: Array<CurrentGameCard> = [];
   numCardsRevealed: number = 0;
   revealedCard: any | null;
+  loadingGame: boolean = true;
 
   constructor(
     fb: FormBuilder,
@@ -40,27 +39,29 @@ export class MatchingComponent {
     this.gameService.getCurrentGame().subscribe((currentGame) => {
       if (currentGame) {
         this.currentGame$ = currentGame;
-        this.cardsArr = currentGame.cards;
-        console.log('Current Game:', this.currentGame$);
-
-        const cardFormGroups = this.initializeCards();
-        const playerFormGroups = this.initializePlayers();
-
-        this.form.setControl('cards', new FormArray(cardFormGroups));
-        this.form.setControl('players', new FormArray(playerFormGroups));
-
-        console.log(this.form.value); // empty cards array and full players array
+        this.initializeForm();
       }
     });
   }
 
-  initializeCards() {
-    console.log('cardsArr: ', this.cardsArr) // logs array with cards in it (not empty)
-    console.log('Is cards an array?', Array.isArray(this.cardsArr)); // logs true
+  async initializeForm() {
+    // initialize FormArray for cards and players
+    const cardFormGroups = await this.initializeCards();
+    const playerFormGroups = this.initializePlayers();
+
+    this.form.setControl('cards', new FormArray(cardFormGroups));
+    this.form.setControl('players', new FormArray(playerFormGroups));
+
+    // once cards and players are initialized, set loadingGame to false
+    this.loadingGame = false;
+  }
+
+  async initializeCards() {
+    // wait for cards to be initialized
+    await this.waitForCards();
 
     // set up FormArray
-    const cardFormGroups = this.cardsArr.map(card => {
-      console.log('card: ', card) // logs nothing
+    return this.currentGame$.cards.map((card) => {
       const newGroup = this.fb.group({
         id: new FormControl(card.id),
         name: new FormControl(card.name),
@@ -74,12 +75,20 @@ export class MatchingComponent {
         })
       });
 
-      console.log(newGroup) // logs nothing
       return newGroup
-    });
+    })
+  }
 
-    console.log('Card Form Groups:', cardFormGroups); // logs empty array
-    return cardFormGroups;
+  async waitForCards() {
+    const startTime = Date.now();
+
+    while (!this.currentGame$.cards[0]) {
+      if (Date.now() - startTime > 10000) {
+        throw new Error('Timeout exceeded');
+      }
+  
+      await new Promise(resolve => setTimeout(resolve, 100));
+    }
   }
 
   initializePlayers() {
